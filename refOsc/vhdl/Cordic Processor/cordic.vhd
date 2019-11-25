34e8bbb5b4dfc04a9e4751a912ca2df98219e877
aeschi2305 <dennis.aeschbacher@students.fhnw.ch>
2019-11-25 09:51:18 +0100

    changed cordic processor and control

--- refOsc/vhdl/Cordic Processor/cordic.vhd
+++ refOsc/vhdl/Cordic Processor/cordic.vhd
@@ -1,5 +1,5 @@
 -----------------------------------------------------
--- Project : Cordic Calculation N iterations parallel
+-- Project : Digital Theremin
 -----------------------------------------------------
 -- File    : cordic.vhd
 -- Author  : dennis.aeschbacher@students.fhnw.ch
@@ -12,9 +12,8 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity Cordic is
-eneric (
+  generic (
     N : natural := 16 --Number of Bits of the sine wave (precision)
-
   );
   port(
     reset_n : in std_ulogic;
@@ -25,78 +24,93 @@ eneric (
 end entity Cordic;
 
 architecture behavioral of Cordic is
-type xyzPar is array (N-1 downto 0) of signed (N-1 downto 0);
-signal xPar : xyzPipeline;
-signal yPar : xyzPipeline;
-signal zPar : xyzPipeline;
+type atan_type is array (0 to 14) of signed (N-1 downto 0);
+constant atan : atan_type :=     ("0100000000000000",  -- Lookuptable for the arcustangent of 2^-k
+                                  "0010010111001000",
+                                  "0001001111110110",
+                                  "0000101000100010",
+                                  "0000010100010110",
+                                  "0000001010001011",
+                                  "0000000101000101",
+                                  "0000000010100010",
+                                  "0000000001010001",
+                                  "0000000000101000",
+                                  "0000000000010100",
+                                  "0000000000001010",
+                                  "0000000000000101",
+                                  "0000000000000010",
+                                  "0000000000000001");
 
-signal xreg : signed (N-1 downto 0);
+--type xyzPar is array (N downto 0) of signed (N-1 downto 0);
+--signal xPar : xyzPar;
+--signal yPar : xyzPar;
+--signal zPar : xyzPar;
+signal ycmb : signed (N-1 downto 0);
+
+--signal xreg : signed (N-1 downto 0);    --only needed if pipelined
 signal yreg : signed (N-1 downto 0);
-signal zreg : signed (N-1 downto 0);
+--signal zreg : signed (N-1 downto 0);    --only needed if pipelined
 
-procedure calculateIteration (
-      xk    : in signed(N-1 downto 0);   -- input x-coordinates
-      yk    : in signed(N-1 downto 0);   -- input y-coordinates
-      zk    : in signed(N-1 downto 0);   -- input angle
-      k     : in natural range 0 to N-1;   -- iteration index
-      xkk   : out signed(N-1 downto 0);   -- output x-coordinates
-      ykk   : out signed(N-1 downto 0);   -- output y-coordinates
-      zkk   : out signed(N-1 downto 0)   -- output angle
-    ) return std_ulogic_vector is
-    type atan_type is array (0 to 14) of signed (N-1 downto 0);
-    constant atan : atan_type :=     ("1100000000000000"  -- Lookuptable for the arcustangent of 2^-k
-                      "1010010111001000"
-                      "1001001111110110"
-                      "1000101000100010"
-                      "1000010100010110"
-                      "1000001010001011"
-                      "1000000101000101"
-                      "1000000010100010"
-                      "1000000001010001"
-                      "1000000000101000"
-                      "1000000000010100"
-                      "1000000000001010"
-                      "1000000000000101"
-                      "1000000000000010"
-                      "1000000000000001"
-);
+
+
+
+
+procedure calculateIteration (        --calculation of one Iteration
+      x   : inout signed(N-1 downto 0);   -- output x-coordinates
+      y   : inout signed(N-1 downto 0);   -- output y-coordinates
+      z   : inout signed(N-1 downto 0);   -- output angle
+      k   : in natural range 0 to N   -- iteration index
+      
+
+)is
+
   begin
-    if zk > '0' then
-        xkk <= xk - shift_right(yk,k);
-        ykk <= yk + shift_right(xk,k);
-        zkk <= zk - atan(k)
+    if z(z'left) = '0' then           -- is zk positive
+        x := x - shift_right(y,k);    
+        y := y + shift_right(x,k);
+        z := z - atan(k);
     else
-        xkk <= xk + shift_right(yk,k);
-        ykk <= yk - shift_right(yk,k);
-        zkk <= zk + atan(k)
+        x := x + shift_right(y,k);
+        y := y - shift_right(x,k);
+        z := z + atan(k);
     end if;
 
   end procedure calculateIteration;
 
 begin
 
-    p_reg : process(reset,clk)
+    p_reg : process(reset_n,clk)
     begin
         if reset_n = '0' then
-
+            --xreg <= (others => '0');    --only needed if pipelined
+            yreg <= (others => '0');
+            --zreg <= (others => '0');    --only needed if pipelined
         elsif rising_edge(clk) then
-            xreg <= xPar(0); 
-            yreg <= yPar(0);
-            zreg <= zPar(0);
+            --xreg <= xPar(0);            --only needed if pipelined
+            yreg <= ycmb;
+            --zreg <= zPar(0);            --only needed if pipelined
         end if;
     end process p_reg;
 
     p_cmb : process(all)
+        variable x : signed(N-1 downto 0);
+        variable y : signed(N-1 downto 0);
+        variable z : signed(N-1 downto 0);
     begin
-        xPip1(15) <= 1;
-        yPip1(15) <= 0;
-        zPip1(15) <= phi;   
-        l_cordic1 : for k in 15 downto 0 loop
-            calculateIteration(xPar(k+1),yPar(k+1),zPar(k+1),k,xPar(k),yPar(k),zPar(k))
+
+        x := (others => '0');
+        x(0) := '1';
+        y := (others => '0');
+        z := phi;   
+        l_cordic1 : for k in N-1 downto 0 loop
+            calculateIteration(x,y,z,k);
         end loop l_cordic1;
+        --xcmb <= x;
+        ycmb <= y;
+        --zcmb <= z;
 
     end process p_cmb;
 
-    sine <= yPip3(0)
+    sine <= yreg;
             
 end architecture behavioral;
--- refOsc/vhdl/Cordic Processor/cordic_verify.vhd
+++ refOsc/vhdl/Cordic Processor/cordic_verify.vhd
@@ -12,10 +12,10 @@ use ieee.numeric_std.all;
 library std;
 use std.textio.all;
 
-entity verify is
+entity cordic_verify is
 	generic (
 	 N : natural := 16;	--Number of Bits of the sine wave (precision)
-	 n : natural := 100	--Number of calculated sine values from -pi/2 to pi/2
+	 nn : integer := 100	--Number of calculated sine values from -pi/2 to pi/2
 	);
   	port(
      reset_n : out std_ulogic;
@@ -23,41 +23,42 @@ entity verify is
      phi : out signed(N-1 downto 0);
      sine : in signed(N-1 downto 0)
   	);
-end entity verify;
+end entity cordic_verify;
 
-architecture stimuli_and_monitor of verify is
+architecture stimuli_and_monitor of cordic_verify is
   constant c_cycle_time : time := 20 ns;
+  constant sig_freq : natural := 500000;
+  constant clk_Period : natural := 1/50000000;
+  constant slope_ang    : signed (N-1 downto 0) := to_signed(2*(2**(N)-1)/nn,N);
   signal enable         : boolean := true;
   signal phi_n          : signed (N-1 downto 0);
-  signal slope_ang 		: signed (N-1 downto 0) := to_signed(2*2**(N-1)/sig_Period,slope_ang'length);
-  signal count 			: natural -n/2 to n/2;
+  signal count 			: integer range 0 to nn/2;
  
 begin
-
-  -- System reset:
-  rst_n <= transport '0', '1' after 10 ns;
 
   p_calculate_phi : process
     
   begin
-  	count = -n/2;
-    while count < n/2 loop
-      phi_n = to_signed(count, slope_ang'length) * slope_ang;
+    wait for 5*c_cycle_time;
+    enable <= true;
+  	count <= -nn/4;
+    while count < nn/4 loop
+      phi_n <= to_signed(count, slope_ang'length) * slope_ang;
       wait for c_cycle_time;
-      count = count + 1;
+      count <= count + 1;
     end loop;
-    enable = false;
+    enable <= false;
   end process p_calculate_phi;
 
 
   -- 50MHz
   p_system_clk : process
   begin
-  	reset_n <= transport '0', '1' after 3*c_clk_cycle;
-  	enable <= true;
+  	reset_n <= transport '0', '1' after 3*c_cycle_time;
+  	
     while enable loop
-      clk <= transport '0', '1' after c_clk_cycle/2;
-      wait for c_clk_cycle;
+      clk <= transport '0', '1' after c_cycle_time/2;
+      wait for c_cycle_time;
     end loop;
     wait;  -- don't do it again
   end process p_system_clk;
